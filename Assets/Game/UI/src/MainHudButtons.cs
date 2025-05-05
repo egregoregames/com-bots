c5c31eaaf05ad1ddc298f8d4151c9c6ba7f2ed74
Kyle Cuss <cussprogramming@gmail.com>
2025-05-05 13:13:40 -0230

    Color sampler extracted as utility class

--- Assets/Game/UI/src/MainHudButtons.cs
+++ Assets/Game/UI/src/MainHudButtons.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Game.UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
@@ -12,7 +13,6 @@ public class MainHudButtons : MonoBehaviour
     [SerializeField] MenuDescriptionPanel menuDescriptionPanel;
     private RectTransform rectTransform;
     private Vector2 initialPosition;
-    private Color buttonColor;
 
     public MenuPanel activePanel;
     private void Awake()
@@ -79,32 +79,6 @@ public class HudButtonToMenuPair
             menuPanelDescription.SetDescription(menuPanel, _spriteColorSampler.spriteColor);
         };
         
-    }
-
-    
-}
-
-public class SpriteColorSampler
-{
-    public Color spriteColor;
-
-    public SpriteColorSampler(Sprite sprite)
-    {
-        // Get the sprite's texture
-        Texture2D texture = sprite.texture;
-
-        // Convert the sprite's rect position to texture-space coordinates
-        Rect rect = sprite.textureRect;
-        
-        // Convert top-left in sprite rect to texture pixel coordinates
-        int x = Mathf.FloorToInt(rect.x);
-        int y = Mathf.FloorToInt(rect.y + rect.height - 1); // Top-left (Y is inverted in Unity textures)
-
-
-        // Read the color at that pixel
-        spriteColor = texture.GetPixel(x, y);
-
-        Debug.Log("Cached color: " + spriteColor);
     }
 }
 
