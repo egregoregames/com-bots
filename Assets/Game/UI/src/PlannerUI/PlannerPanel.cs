698053a00e0d1abea449452fe8074be646ce87e8
Kyle Cuss <cussprogramming@gmail.com>
2025-05-12 14:55:01 -0230

    addressed new/completed quest states

--- Assets/Game/UI/src/PlannerUI/PlannerPanel.cs
+++ Assets/Game/UI/src/PlannerUI/PlannerPanel.cs
@@ -9,7 +9,7 @@ namespace Game.UI.src.PlannerUI
 {
     public class PlannerPanel: MenuPanel
     {
-        [SerializeField] List<MenuTab> subTabs = new();
+        [SerializeField] List<PlannerTab> subPlannerTabs = new();
         [SerializeField] PlannerCategoryTab requirementsTab;
         [SerializeField] PlannerCategoryTab electivesTab;
         [SerializeField] TextMeshProUGUI questNameText;
@@ -18,30 +18,58 @@ namespace Game.UI.src.PlannerUI
         [SerializeField] TextMeshProUGUI questGiverNameText;
         [SerializeField] Image questGiverImage;
         [SerializeField] ScrollRect scrollRect;
+        List<MenuTab> subMenuTabs = new();
         int _currentSubIndex;
         int _currentCategoryIndex;
 
         void Awake()
         {
+            SetAllButtonActions();
+            subMenuTabs.AddRange(subPlannerTabs);
+        }
+
+        void SetAllButtonActions()
+        {
             SetButtonOnSelect(requirementsTab, SetRequirementsAction);
             SetButtonOnSelect(electivesTab, SetElectivesAction);
+
+            for (int i = 0; i < subPlannerTabs.Count; i++)
+            {
+                SetButtonOnSelect(subPlannerTabs[i], () => SetQuestTabAction(i));
+            }
         }
 
         void SetRequirementsAction()
         {
             //TODO: Adjust sub tabs to only have active as many requirement quests as player has, set all quest names
+            foreach (var subTab in subPlannerTabs) // pull whether quest is complete or brand new from quest data
+            {
+                subTab.SetQuestStatuses(true, false);
+            }
         }
 
         void SetElectivesAction()
         {
             //TODO: Adjust sub tabs to only have active as many elective quests as player has, set all quest names
+            foreach (var subTab in subPlannerTabs) // pull whether quest is complete or brand new from quest data
+            {
+                subTab.SetQuestStatuses(false, true);
+            }
+        }
+        
+        void SetQuestTabAction(int questIndex)
+        {
+            //TODO: Figure out requirements quest list vs. electives quest list
+            //TODO: Set description panel values.  Quest name, quest description text, quest points, quest giver name, quest giver image
+            //TODO: Set is new quest to false 
         }
         
         public override void OpenMenu()
         {
             base.OpenMenu();
+            SetRequirementsAction();
             ResetSubButtons();
-            SetQuestInfo(0);
+            SetQuestTabAction(0);
         
             inputSO.OnLeft += HandleLeftInput;
             inputSO.OnRight += HandleRightInput;
@@ -61,33 +89,31 @@ namespace Game.UI.src.PlannerUI
         void HandleLeftInput()
         {
             SetActiveSubButton(categoryButtons, -1, ref _currentCategoryIndex);
-            StartCoroutine(ResetScrollPositionAndSelect(scrollRect, subTabs, 0));
+            StartCoroutine(ResetScrollPositionAndSelect(scrollRect, subMenuTabs, 0));
             ResetSubButtons();
         }
 
         void HandleRightInput()
         {
             SetActiveSubButton(categoryButtons, 1, ref _currentCategoryIndex);
-            StartCoroutine(ResetScrollPositionAndSelect(scrollRect, subTabs, 0));
+            StartCoroutine(ResetScrollPositionAndSelect(scrollRect, subMenuTabs, 0));
             ResetSubButtons();
         }
 
         void HandleUpInput()
         {
-            SetActiveSubButton(subTabs, -1, ref _currentSubIndex);
-            SetQuestInfo(_currentSubIndex);
+            SetActiveSubButton(subMenuTabs, -1, ref _currentSubIndex);
         }
 
         void HandleDownInput()
         {
-           SetActiveSubButton(subTabs, 1, ref _currentSubIndex);
-           SetQuestInfo(_currentSubIndex);
+           SetActiveSubButton(subMenuTabs, 1, ref _currentSubIndex);
         }
         
         void ResetSubButtons()
         {
             _currentSubIndex = -1;
-            SetActiveSubButton(subTabs, 1, ref _currentSubIndex);
+            SetActiveSubButton(subMenuTabs, 1, ref _currentSubIndex);
         }
         
         void SetActiveSubButton(List<MenuTab> tabs, int direction, ref int currentIndex)
@@ -116,28 +142,18 @@ namespace Game.UI.src.PlannerUI
         
         IEnumerator ResetScrollPositionAndSelect(ScrollRect scrollRect, List<MenuTab> tabs, int selectIndex)
         {
-            // Step 1: Force layout to be updated
             Canvas.ForceUpdateCanvases();
-
-            // Step 2: Reset scroll
+            
             scrollRect.normalizedPosition = new Vector2(0, 1);
-
-            // Step 3: Clear selection and wait a frame to let Unity finish UI layout
+            
             EventSystem.current.SetSelectedGameObject(null);
-            yield return null; // let one frame pass
-
-            // Step 4: Re-assign the selected UI element
+            yield return null;
+            
             if (selectIndex >= 0 && selectIndex < tabs.Count)
             {
                 var target = tabs[selectIndex].gameObject;
                 EventSystem.current.SetSelectedGameObject(target);
             }
-        }
-
-        void SetQuestInfo(int questIndex)
-        {
-            //TODO: Figure out requirements quest list vs. electives quest list
-            //TODO: Set description panel values.  Quest name, quest description text, quest points, quest giver name, quest giver image
         }
     }
 }
--- Assets/Game/UI/src/PlannerUI/PlannerTab.cs
+++ Assets/Game/UI/src/PlannerUI/PlannerTab.cs
@@ -1,15 +1,31 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game.UI.src.PlannerUI
 {
     public class PlannerTab : MenuTab
     {
+        const string CompletedColorString = "#8D989E";
+
         RectTransform rectTransform;
+        GameObject newCircleGameObject;
+        GameObject completedLabelGameObject;
+        Image buttonBackground;
+        TextMeshProUGUI buttonText;
         Vector2 initialPosition;
+        Color _completedBackgroundColor;
         
         protected override void Awake()
         {
+            base.Awake();
+            
+            ColorUtility.TryParseHtmlString(CompletedColorString, out _completedBackgroundColor);
             rectTransform = transform.GetComponent<RectTransform>();
+            buttonBackground = transform.GetComponent<Image>();
+            buttonText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+            completedLabelGameObject = transform.GetChild(1).gameObject;
+            newCircleGameObject = transform.GetChild(2).gameObject;
             initialPosition = rectTransform.anchoredPosition; // Store initial UI position
         }
         
@@ -20,6 +36,7 @@ namespace Game.UI.src.PlannerUI
             if(!rectTransform || !rectTransform.anchoredPosition.Equals(initialPosition)) return;
         
             rectTransform.anchoredPosition += new Vector2(offset, 0);
+            newCircleGameObject.SetActive(false);
             onSelect?.Invoke();
         }
         
@@ -27,6 +44,24 @@ namespace Game.UI.src.PlannerUI
         public override void DeselectEffect()
         {
             rectTransform.anchoredPosition = initialPosition;
+        }
+
+        public void SetQuestStatuses(bool isNewQuest, bool isCompleted)
+        {
+            newCircleGameObject.SetActive(isNewQuest);
+
+            if (isCompleted)
+            {
+                completedLabelGameObject.SetActive(true);
+                buttonBackground.color = _completedBackgroundColor;
+                buttonText.color = Color.white;
+            }
+            else
+            {
+                completedLabelGameObject.SetActive(false);
+                buttonBackground.color = Color.white;
+                buttonText.color = Color.black;
+            }
         }
     }
 }
