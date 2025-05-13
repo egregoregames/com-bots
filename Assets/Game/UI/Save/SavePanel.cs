f6b24e62cfa680fc2dcf805d8c20b7d6b60d876b
Kyle Cuss <cussprogramming@gmail.com>
2025-05-13 13:50:06 -0230

    Setup save panel GUI

---
+++ Assets/Game/UI/Save/SavePanel.cs
@@ -0,0 +1,7 @@
+namespace Game.UI.Save
+{
+    public class SavePanel : MenuPanel
+    {
+    
+    }
+}
---
+++ Assets/Game/UI/Save/SaveTab.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+namespace Game.UI.Save
+{
+    public class SaveTab : MenuTab
+    {
+        [SerializeField] TextMeshProUGUI saveDescription;
+        
+        
+    }
+}
