c8fbe9b80d4f6315d8b76ce4d3b44a14db4bd469
Kyle Cuss <cussprogramming@gmail.com>
2025-06-24 07:21:18 -0400

    top hud updates on open/close

--- Assets/Game/Managers/PlayerData.cs
+++ Assets/Game/Managers/PlayerData.cs
@@ -10,5 +10,11 @@ public class PlayerData : ScriptableObject
     public int ownedBlueprints = 5;
     public string playerOccupation = "First-Year Student";
     public int rank  = 1;
+    public int requiredRankExperience = 50;
+    public int currentRankExperience = 12;
     public string playerName = "Player";
+    public string termName = "Early Fall";
+    public float termCredits = 10.5f;
+    public int currency = 1000;
+    public string currentLocation = "Beach";
 }
---
+++ Assets/Game/UI/src/HUDUI/TopHUDController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI.src.HUDUI
+{
+    public class TopHUDController : MonoBehaviour
+    {
+        [SerializeField] PlayerData playerData;
+        [SerializeField] TextMeshProUGUI clockText;
+        [SerializeField] TextMeshProUGUI creditsText;
+        [SerializeField] TextMeshProUGUI currencyText;
+        [SerializeField] TextMeshProUGUI locationText;
+        [SerializeField] TextMeshProUGUI rankText;
+        [SerializeField] Slider rankExperienceSlider;
+
+        void OnEnable()
+        {
+            StartCoroutine(StartTime());
+            UpdateHUD();
+        }
+
+        void OnDisable()
+        {
+            StopAllCoroutines();
+        }
+        
+        IEnumerator StartTime()
+        {
+            while (true)
+            {
+                string timeText = System.DateTime.Now.ToString("ddd h:mm tt");
+                clockText.text = $"{timeText} ({playerData.termName})" ;
+                yield return new WaitForSeconds(1f); // update once per second
+            }
+        }
+
+        void UpdateHUD()
+        {
+            UpdateCredits();
+            UpdateCurrency();
+            UpdateLocation();
+            UpdateRank();
+            UpdateRankExperience();
+        }
+
+        void UpdateCredits()
+        {
+            creditsText.text = playerData.termCredits.ToString(CultureInfo.InvariantCulture);
+        }
+
+        void UpdateCurrency()
+        {
+            currencyText.text = playerData.currency.ToString("N0", CultureInfo.InvariantCulture);;
+        }
+
+        void UpdateLocation()
+        {
+            locationText.text = playerData.currentLocation;
+        }
+
+        void UpdateRank()
+        {
+            rankText.text = playerData.rank.ToString();
+        }
+
+        void UpdateRankExperience()
+        {
+            rankExperienceSlider.maxValue = playerData.requiredRankExperience;
+            rankExperienceSlider.value = playerData.currentRankExperience;
+        }
+    }
+}
