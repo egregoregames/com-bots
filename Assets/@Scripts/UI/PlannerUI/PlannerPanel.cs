664e00ffc0217f1d330c545afac043e5f1a5dfd1
SplenectomY <skaterjohn16@hotmail.com>
2025-10-29 12:28:09 -0400

    Update PlannerPanel.cs
    Preliminary coding/events

--- Assets/@Scripts/UI/PlannerUI/PlannerPanel.cs
+++ Assets/@Scripts/UI/PlannerUI/PlannerPanel.cs
@@ -5,11 +5,48 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using System.Linq;
+using System.Threading.Tasks;
 
-namespace ComBots.UI.src.PlannerUI
+public class PlannerPanel : MonoBehaviourR3
 {
-    public class PlannerPanel : MonoBehaviourR3
+    public static PlannerPanel Instance { get; private set; }
+
+    [field: SerializeField]
+    private List<PlannerQuestItem> QuestItems { get; set; }
+
+    [field: SerializeField]
+    private GameObject QuestItemTemplate { get; set; }
+
+    private new void Awake()
     {
-        
+        base.Awake();
+        QuestItems.Clear();
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        Instance = this;
+
+        AddEvents(
+            ComBotsSaveSystem.OnLoadSuccess(RefreshQuestItems));
+    }
+
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private async void RefreshQuestItems()
+    {
+        QuestItems.ForEach(x => Destroy(x));
+        QuestItems.Clear();
+
+        var gameData = await PersistentGameData.GetInstanceAsync();
+
+        foreach (var item in gameData.PlayerQuestTrackingData)
+        {
+            
+        }
     }
 }
