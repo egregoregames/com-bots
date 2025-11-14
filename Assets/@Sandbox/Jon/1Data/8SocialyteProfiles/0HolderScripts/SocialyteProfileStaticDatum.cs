a21a54301c261d0031e86b6ff9ac82dc8206e6c6
SplenectomY <skaterjohn16@hotmail.com>
2025-11-14 10:58:12 -0500

    Update SocialyteProfileStaticDatum.cs
    Add XML comments and tooltips

--- Assets/@Sandbox/Jon/1Data/8SocialyteProfiles/0HolderScripts/SocialyteProfileStaticDatum.cs
+++ Assets/@Sandbox/Jon/1Data/8SocialyteProfiles/0HolderScripts/SocialyteProfileStaticDatum.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 using UnityEngine.Serialization;
 
+/// <summary>
+/// Contains immutable data for individual entires in the Socialyte App in the 
+/// pause menu. Accessible via <see cref="StaticGameData.SocialyteData"/>
+/// </summary>
 [CreateAssetMenu(fileName = "SocialyteProfileStaticDatum", 
 	menuName = "Scriptable Objects/Socialyte Profile Static Datum")]
 public class SocialyteProfileStaticDatum : ScriptableObject
 {
-	[field: SerializeField]
+	/// <summary>
+	/// Must be unique
+	/// </summary>
+	[field: SerializeField, Tooltip("Must be unique")]
 	public int ProfileId { get; private set; }
 
 	[field: SerializeField, FormerlySerializedAs("profileName")]
@@ -17,7 +24,10 @@ public class SocialyteProfileStaticDatum : ScriptableObject
     [field: SerializeField, FormerlySerializedAs("modelOverworld")]
     public GameObject ModelOverworld { get; private set; }
 
-	[field: SerializeField, FormerlySerializedAs("potentialTeammate"), 
+    /// <summary>
+    /// Whether this NPC can also be a teammate
+    /// </summary>
+    [field: SerializeField, FormerlySerializedAs("potentialTeammate"), 
 		Tooltip("Whether this NPC can also be a teammate.")]
 	public bool IsPotentialTeammate { get; private set; }
 
