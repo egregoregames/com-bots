a366b06bb4293bae0739fd5fb160c6498a5480f8
SplenectomY <skaterjohn16@hotmail.com>
2025-11-14 10:47:06 -0500

    Update dataHolderSocialyteProfile.cs
    Refactor members to use proper naming conventions, access control for static data

--- Assets/@Sandbox/Jon/1Data/8SocialyteProfiles/0HolderScripts/dataHolderSocialyteProfile.cs
+++ Assets/@Sandbox/Jon/1Data/8SocialyteProfiles/0HolderScripts/dataHolderSocialyteProfile.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
-[CreateAssetMenu(fileName = "dataHolderSocialyteProfile", menuName = "Scriptable Objects/dataHolderSocialyteProfile")]
-
+[CreateAssetMenu(fileName = "dataHolderSocialyteProfile", 
+	menuName = "Scriptable Objects/dataHolderSocialyteProfile")]
 public class dataHolderSocialyteProfile : ScriptableObject
 {
-	public string profileName;
-	public GameObject modelPortrait;
-	public GameObject modelOverworld;
-	[Tooltip("Whether this NPC can also be a teammate.")]
-	public bool potentialTeammate;
-	public string occupation;
-	public string origin;
-	[TextArea(3, 10)]
-	public string bio;
+	[field: SerializeField]
+	public int ProfileId { get; private set; }
+
+	[field: SerializeField, FormerlySerializedAs("profileName")]
+	public string ProfileName { get; private set; }
+
+	[field: SerializeField, FormerlySerializedAs("modelPortrait")]
+	public GameObject ModelPortrait { get; private set; }
+
+    [field: SerializeField, FormerlySerializedAs("modelOverworld")]
+    public GameObject ModelOverworld { get; private set; }
+
+	[field: SerializeField, FormerlySerializedAs("potentialTeammate"), 
+		Tooltip("Whether this NPC can also be a teammate.")]
+	public bool IsPotentialTeammate { get; private set; }
+
+	[field: SerializeField, FormerlySerializedAs("occupation")]
+	public string Occupation { get; private set; }
+
+	[field: SerializeField, FormerlySerializedAs("origin")]
+    public string Origin { get; private set; }
+
+	[field: SerializeField, FormerlySerializedAs("bio"), TextArea(3, 10)]
+	public string Bio { get; private set; }
 }
