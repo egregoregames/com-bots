d808addd5cb841a55dc411556ef9c90fadffd6ba
SplenectomY <skaterjohn16@hotmail.com>
2025-11-16 15:17:33 -0500

    Clean up PersistentGameData.cs
    - Remove unused usings
    - Move GameEvents subclass to separate script

--- Assets/@Scripts/Player/PersistentGameData/PersistentGameData.cs
+++ Assets/@Scripts/Player/PersistentGameData/PersistentGameData.cs
@@ -1,15 +1,9 @@
 using ComBots.Game.Portals;
-using Mono.Cecil.Cil;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Unity.Mathematics;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 /// <summary>
 /// Singleton that must exist as soon as the game starts. Stores frequently 
@@ -32,46 +26,6 @@ public partial class PersistentGameData : MonoBehaviourR3
     private static UnityEventR3<QuestTrackingDatum> _onQuestUpdated = new();
     private static UnityEventR3<InventoryItemDatum> _onInventoryItemUpdated = new();
     private static UnityEventR3<int> _onSocialyteProfileAdded = new();
-
-    public static class GameEvents
-    {
-        public static IDisposable OnTermUpdated(Action x)
-            => _onTermUpdated.Subscribe(x);
-
-        public static IDisposable OnSaveDataLoaded(Action x)
-            => _onSaveDataLoaded.Subscribe(x);
-
-        public static IDisposable OnRankXpUpdated(Action x)
-            => _onRankXpUpdated.Subscribe(x);
-
-        public static IDisposable OnCreditsUpdated(Action x)
-            => _onCreditsUpdated.Subscribe(x);
-
-        public static IDisposable OnMoneyUpdated(Action x)
-            => _onMoneyUpdated.Subscribe(x);
-
-        public static IDisposable OnLocationUpdated(Action x)
-            => _onLocationUpdated.Subscribe(x);
-
-        public static IDisposable OnQuestUpdated(Action<QuestTrackingDatum> x)
-            => _onQuestUpdated.Subscribe(x);
-
-        public static IDisposable OnInventoryItemUpdate(Action<InventoryItemDatum> x)
-            => _onInventoryItemUpdated.Subscribe(x);
-
-        /// <summary>
-        /// Is invoked when a new connection is made in the Socialyte App. 
-        /// Passes the integer ID of the NPC
-        /// </summary>
-        /// 
-        /// <param name="x">
-        /// An action with the integer ID of the added NPC as a parameter
-        /// </param>
-        /// 
-        /// <returns></returns>
-        public static IDisposable OnSocialyteProfileAdded(Action<int> x)
-            => _onSocialyteProfileAdded.Subscribe(x);
-    }
 
     /// <summary>
     /// Entered by the player at the start of a new game
