3fc541355b3e00a418ddc60a2d6a279a09353f3e
SplenectomY <skaterjohn16@hotmail.com>
2025-11-16 17:09:39 -0500

    Move Inventory API
    Create subclass to contain Inventory API methods and reduce the "god script" attributes of PersistentGameData

--- Assets/@Scripts/Player/PersistentGameData/PersistentGameData.cs
+++ Assets/@Scripts/Player/PersistentGameData/PersistentGameData.cs
@@ -344,91 +344,6 @@ public partial class PersistentGameData : MonoBehaviourR3
     }
 
     /// <summary>
-    /// Add an item to the player's inventory. Will throw a warning if you 
-    /// exceed the item's max quantity. This is very undesirable. 
-    /// Check <see cref="StaticGameData.GetMaxInventoryItemQuantity(int)"/> 
-    /// and <see cref="GetInventoryAmount(int)"/> first
-    /// </summary>
-    /// <param name="itemId"></param>
-    /// <param name="amount">How many of the item to add. Will throw a warning 
-    /// if you exceed the item's max quantity. This is very undesirable. 
-    /// Check <see cref="StaticGameData.GetMaxInventoryItemQuantity(int)"/> 
-    /// and <see cref="GetInventoryAmount(int)"/> first
-    /// </param>
-    public static async void AddInventoryItem(int itemId, int amount)
-    {
-        var instance = await GetInstanceAsync();
-
-        var item = instance.PlayerInventoryItemData
-            .FirstOrDefault(x => x.ItemId == itemId);
-
-        if (item == null)
-        {
-            item = new InventoryItemDatum()
-            {
-                ItemId = itemId
-            };
-
-            instance.PlayerInventoryItemData.Add(item);
-        }
-
-        var staticData = await item.GetStaticDataAsync();
-        item.Quantity += amount;
-
-        if (staticData.MaxQuantity < item.Quantity)
-        {
-            item.Quantity = staticData.MaxQuantity;
-
-            var message = $"Tried to add too many of item type " +
-                $"{staticData.ItemName} to player inventory";
-
-            Instance.Log(message, LogLevel.Warning);
-        }
-
-        _onInventoryItemUpdated?.Invoke(item);
-    }
-
-    /// <summary>
-    /// Remove an item from the player's inventory. Will log a warning if the 
-    /// end quantity is lower than 0. This would be very undsirable. 
-    /// Check <see cref="GetInventoryAmount"/> first
-    /// </summary>
-    /// <param name="itemId"></param>
-    /// <param name="amount">The amount to remove. Will log a warning if the 
-    /// end quantity is lower than 0. This would be very undsirable. 
-    /// Check <see cref="GetInventoryAmount"/> first</param>
-    public static async void RemoveInventoryItem(int itemId, int amount)
-    {
-        var instance = await GetInstanceAsync();
-
-        var item = instance.PlayerInventoryItemData
-            .FirstOrDefault(x => x.ItemId == itemId);
-
-        if (item == null)
-        {
-            string message = $"Tried to remove an item from the player's " +
-                $"inventory (ID: {itemId}) that did not exist";
-
-            Instance.Log(message, LogLevel.Warning);
-            return;
-        }
-
-        item.Quantity -= amount;
-
-        if (item.Quantity < 0)
-        {
-            string message = $"Took too many of item {item.ItemId} from " +
-                $"player's inventory";
-
-            Instance.Log(message, LogLevel.Warning);
-
-            item.Quantity = 0;
-        }
-
-        _onInventoryItemUpdated?.Invoke(item);
-    }
-
-    /// <summary>
     /// Add a new NPC connection in the Socialyte app. Will log a warning if
     /// the NPC already exists as a connection. Check if the connection already exists
     /// by calling <see cref="PlayerNpcConnections.Contains(npcId)"/>
@@ -446,20 +361,6 @@ public partial class PersistentGameData : MonoBehaviourR3
         }
 
         instance.PlayerNpcConnections.Add(npcId);
-    }
-
-    /// <returns>An integer representing how many of an itemId the user has</returns>
-    public static int GetInventoryAmount(int itemId)
-    {
-        var item = Instance.PlayerInventoryItemData
-            .FirstOrDefault(x => x.ItemId == itemId);
-
-        if (item == null)
-        {
-            return 0;
-        }
-
-        return item.Quantity;
     }
 
     private void GenerateStudentIdIfNoneExists()
