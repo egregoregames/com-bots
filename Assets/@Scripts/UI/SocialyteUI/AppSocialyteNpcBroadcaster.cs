d8a499a586a474cf3c29cbd0a8b5911e2cf370da
SplenectomY <skaterjohn16@hotmail.com>
2025-11-20 17:58:17 -0500

    Update AppSocialyteNpcBroadcaster.cs
    Performance / optimization update:
    - NPC Broadcaster now listens for close event from Socialyte app and will automatically disable when the Socialyte app is closed
    - Refactor code (smaller methods)

--- Assets/@Scripts/UI/SocialyteUI/AppSocialyteNpcBroadcaster.cs
+++ Assets/@Scripts/UI/SocialyteUI/AppSocialyteNpcBroadcaster.cs
@@ -25,52 +25,82 @@ public class AppSocialyteNpcBroadcaster : MonoProtectedSingletonR3<AppSocialyteN
     [field: SerializeField]
     private List<NpcBroadcasterDatum> NpcBroadcasterData { get; set; } = new();
 
+    protected override void Awake()
+    {
+        base.Awake();
+        Close();
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        AddEvents(
+            AppSocialyte.OnClosed(Close));
+    }
+
     public static void BroadcastNpc(SocialyteProfileStaticDatum datum)
     {
+        Instance.gameObject.SetActive(true);
+
         Instance.NpcBroadcasterData
             .ForEach(data => data.Instance.SetActive(false));
 
         var existing = Instance.NpcBroadcasterData
             .FirstOrDefault(x => x.NpcId == datum.ProfileId);
 
+        existing ??= AddInstance(datum);
+
         if (existing == null)
-        {
-            if (datum.ModelPortrait == null)
-            {
-                Debug.LogWarning($"No model profile set for npc {datum.ProfileName}");
-                return;
-            }
-            var instance = Instantiate(datum.ModelPortrait, Instance.transform);
-            var components = instance.GetComponentsInChildren<Component>(true);
-
-            Array.ForEach(components, component => 
-            {
-                var type = component.GetType();
-                //Debug.Log(type.ToString());
-
-                if (type != typeof(Transform) 
-                    && type != typeof(SkinnedMeshRenderer) 
-                    && type != typeof(Animator))
-                {
-                    Destroy(component);
-                }
-                else
-                {
-                    component.gameObject.layer = LayerMask.NameToLayer("SocialyteProfile");
-                }
-            });
-
-            instance.transform.localPosition = Vector3.zero;
-
-            existing = new NpcBroadcasterDatum()
-            {
-                Instance = instance,
-                NpcId = datum.ProfileId
-            };
-
-            Instance.NpcBroadcasterData.Add(existing);
-        }
+            return;
 
         existing.Instance.SetActive(true);
+    }
+
+    private static NpcBroadcasterDatum AddInstance(SocialyteProfileStaticDatum datum)
+    {
+        if (datum.ModelPortrait == null)
+        {
+            Debug.LogWarning($"No model profile set for npc {datum.ProfileId} - {datum.ProfileName}");
+            return null;
+        }
+
+        var instance = Instantiate(datum.ModelPortrait, Instance.transform);
+        RemoveUnneededComponents(instance);
+
+        instance.transform.localPosition = Vector3.zero;
+
+        var newDatum = new NpcBroadcasterDatum()
+        {
+            Instance = instance,
+            NpcId = datum.ProfileId
+        };
+
+        Instance.NpcBroadcasterData.Add(newDatum);
+        return newDatum;
+    }
+
+    private static void RemoveUnneededComponents(GameObject instance)
+    {
+        var components = instance.GetComponentsInChildren<Component>(true);
+        Array.ForEach(components, component =>
+        {
+            var type = component.GetType();
+            if (type != typeof(Transform)
+                && type != typeof(SkinnedMeshRenderer)
+                && type != typeof(Animator))
+            {
+                Destroy(component);
+            }
+            else
+            {
+                component.gameObject.layer = LayerMask.NameToLayer("SocialyteProfile");
+            }
+        });
+    }
+
+    private void Close()
+    {
+        gameObject.SetActive(false);
     }
 }
